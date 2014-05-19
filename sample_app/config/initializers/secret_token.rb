f49d7e7c96a43f77de7a0b5b2ae01f162ef1e911
KennyZhu <yanlongzhu2010@gmail.com>
2014-05-18 18:52:52 -0700

    6

--- sample_app/config/initializers/secret_token.rb
+++ sample_app/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'a18faa9aac492790d80755913503e4a0daabc79b0f4f0872bc1864d01a9dade3b7e89f26a50e4da4302023e3936538b1a0a6adb6d79467ec4e39eaeff3a0cfcd'
+SampleApp::Application.config.secret_key_base = '089cb8480a0248f95075a5b3fca5d3df8897dedcd616cde3a011f80d6b7d226edf51c33e02a125628797aa6aca95e2dfff01fbffffb5d38ee28f62d17b0d8f0c'
---
+++ sample_app/spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe User do
+	before do
+		@user=User.new(name:"yanlong",email:"yanlongzhu2010@gmail.com") 
+	end
+
+	subject {@user}
+
+	it {should respond_to(:name)}
+	it {should respond_to(:email)}
+	
+	describe "when name is not present" do
+		before {@user.name = " "}
+                it {should_not be_valid}
+	end
+	describe "when email is not present" do
+		before {@user.email = " "}
+		it {should_not be_valid}
+	end
+	describe "when name is too long " do
+		before {@user.name = "a"*51}
+		it {should_not be_valid}
+	end
+end
